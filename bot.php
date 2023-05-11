af58cb3acb708e8c2de68c292497dffd9e59263f
Jeroen de Nijs <jeroendenijs.k.smile@outlook.com>
2023-05-11 11:22:44 +0200

    Use PHP library instead of Javascript.

---
+++ bot.php
@@ -0,0 +1,33 @@
+<?php
+
+include __DIR__ . '/vendor/autoload.php';
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Intents;
+use Discord\WebSockets\Event;
+use jeroendn\PhpHelpers\EnvHelper;
+
+EnvHelper::loadEnv();
+
+$discord = new Discord([
+    'token'   => getenv('DISCORD_TOKEN'),
+    'intents' => Intents::getDefaultIntents()
+//      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
+]);
+
+$discord->on('ready', function (Discord $discord) {
+    echo "Bot is ready!", PHP_EOL;
+
+    // Listen for messages.
+    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+        if ($message->content === 'ping') {
+            $message->reply('pong');
+        }
+
+        echo "{$message->author->username}: {$message->content}", PHP_EOL;
+        // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+    });
+});
+
+$discord->run();
