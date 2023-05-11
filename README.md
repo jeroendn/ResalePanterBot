7b7365aac51ff880f7c64e29e574fecdfee6bc2c
Jeroen de Nijs <jeroendenijs.k.smile@outlook.com>
2023-05-11 19:16:17 +0200

    Cleanup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Resale Panter Discord bot
+
+## Available commands
+
+- Fuse {pokemonName} {pokemonName} (Create 1/2 fused images of pokemon)
+- Fuse (Create 1/2 fused images of random pokemon)
--- bot.php
+++ bot.php
@@ -13,7 +13,6 @@ EnvHelper::loadEnv(__DIR__ . '/.env');
 $discord = new Discord([
     'token'   => getenv('DISCORD_TOKEN'),
     'intents' => Intents::getDefaultIntents()
-//      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
 ]);
 
 require_once __DIR__ . '/src/pkmn.php';
@@ -21,7 +20,6 @@ require_once __DIR__ . '/src/pkmn.php';
 $discord->on('ready', function (Discord $discord) {
     echo "Bot is ready!", PHP_EOL;
 
-    // Listen for messages.
     $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
         if ($message->content === 'ping') {
             $message->reply('pong');
