21ed13482a36c67c2cd228c1a0c67173688205aa
Jeroen de Nijs <jeroendenijs.k.smile@outlook.com>
2023-05-12 19:04:19 +0200

    Pokemon fusion improvements.

--- run.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$result = shell_exec('php bot.php 2>&1 > running.log');
-
-echo 'Running bot';
-
-print_r($result);
-die;
---
+++ start.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Script to manually start the bot in case it's not being run by the cronjob.
+ */
+
+$result = shell_exec('php bot.php 2>&1 > running.log');
+
+die('Started bot');
