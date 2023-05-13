4734d34164081814733ee2983d2a97d9074ddd2a
Jeroen de Nijs <jeroendenijs.k.smile@outlook.com>
2023-05-13 19:28:57 +0200

    Finally figured out how to set up slash commands.

--- bot.php
+++ bot.php
@@ -4,8 +4,11 @@ require_once __DIR__ . '/src/lock.php';
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Discord\Builders\CommandBuilder;
+use Discord\Builders\MessageBuilder;
 use Discord\Discord;
-use Discord\Parts\Channel\Message;
+use Discord\InteractionType;
+use Discord\Parts\Interactions\Interaction;
 use Discord\WebSockets\Event;
 use Discord\WebSockets\Intents;
 use jeroendn\PhpHelpers\EnvHelper;
@@ -42,18 +45,81 @@ $discord->on('ready', function (Discord $discord) {
                 /(((////  //((/                 //(/((*(((/(/                   
                /((/(((//////(/              /(/(/(////(/(((/(                   \033[0m", PHP_EOL;
 
+    $discord->application->commands->save(
+        $discord->application->commands->create(CommandBuilder::new()
+            ->setName('help')
+            ->setDescription('Get a list of available commands')
+            ->toArray()
+        )
+    );
 
-    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-        echo $message;
-        if (strtolower($message->content) === getenv('COMMAND_CHAR') . 'help') {
-            $message->reply("!fuse {pokemon1} {pokemon2}
-Fuse 2 Pokemon
+    $discord->application->commands->save(
+        $discord->application->commands->create(CommandBuilder::new()
+            ->setName('ping')
+            ->setDescription('Check bot online status')
+            ->toArray()
+        )
+    );
+
+//    $discord->application->commands->save(
+//        $discord->application->commands->create(CommandBuilder::new()
+//            ->setName('fuse')
+//            ->setDescription('Fuse 2 Pokemon')
+//            ->toArray()
+//        )
+//    );
+
+//    $discord->application->commands->save(
+//        $discord->application->commands->create(CommandBuilder::new()
+//            ->setName('fuserandom')
+//            ->setDescription('Fuse 2 random Pokemon')
+//            ->toArray()
+//        )
+//    );
+
+    $discord->on(Event::INTERACTION_CREATE, function (Interaction $interaction, Discord $discord) {
+        if ($interaction->type === InteractionType::APPLICATION_COMMAND) {
+
+            switch ($interaction->data->name) {
+                case 'help':
+                    $message = MessageBuilder::new()->setContent("!fuse {pokemon1} {pokemon2}
+Fuse 2 Pokemon (Not available as slash command yet)
 
 !fuse
-Fuse 2 random Pokemon
+Fuse 2 random Pokemon (Not available as slash command yet)
 
-!ping
+/ping
 Check bot status");
+
+                    try {
+                        $interaction->respondWithMessage($message);
+                    }
+                    catch (Throwable $e) {
+                        echo $e->getMessage();
+                    }
+
+                    break;
+                case 'ping':
+                    $message = MessageBuilder::new()->setContent('ping');
+
+                    try {
+                        $interaction->respondWithMessage($message);
+                    }
+                    catch (Throwable $e) {
+                        echo $e->getMessage();
+                    }
+
+                    break;
+                case 'fuse':
+                    // TODO
+
+                    break;
+                case 'fuserandom':
+                    // TODO
+
+                    break;
+            }
+
         }
     });
 
--- src/Integration/PingPong/pingpong.php
+++ src/Integration/PingPong/pingpong.php
@@ -7,12 +7,12 @@ use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
 
-$discord->on('ready', function (Discord $discord) {
-
-    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-        if (strtolower($message->content) === getenv('COMMAND_CHAR') . 'ping') {
-            $message->reply('pong');
-        }
-    });
-
-});
+//$discord->on('ready', function (Discord $discord) {
+//
+//    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+//        if (strtolower($message->content) === getenv('COMMAND_CHAR') . 'ping') {
+//            $message->reply('pong');
+//        }
+//    });
+//
+//});
