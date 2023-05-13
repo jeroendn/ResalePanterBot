20198113fc05bfeb1240c5e6e21c09df8c2233d5
Jeroen de Nijs <jeroendenijs.k.smile@outlook.com>
2023-05-13 19:33:45 +0200

    Fix manual start script.

--- start.php
+++ start.php
@@ -3,6 +3,6 @@
  * Script to manually start the bot in case it's not being run by the cronjob.
  */
 
-$result = proc_open('php bot.php 2>&1 > /logs/bot.log', [], $pipes);
+$result = proc_open('php bot.php 2>&1 &', [], $pipes);
 
 die('Bot is running');
